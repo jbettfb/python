d725458b2702ad1e61d64953dffb6ca1033534f1
jbettfb <joao.pedro.sesi2017@gmail.com>
2024-09-28 10:48:20 -0300

    fix: adicionado . no return calculo imc

--- imc.py
+++ imc.py
@@ -4,16 +4,16 @@ def calculoImc(peso, altura):
     
 
     if imc < 16.9:
-        return "muito abaixo do peso"
+        return "muito abaixo do peso."
     elif imc >= 17 and imc < 18.4:
-        return "abaixo do peso"
+        return "abaixo do peso."
     elif imc >= 18.5 and imc <24.9:
-        return "com o peso normal"
+        return "com o peso normal."
     elif imc >= 25 and imc <= 29.9:
-        return "acima do peso"
+        return "acima do peso."
     elif imc >= 30 and imc <= 34.4:
-        return "com obesidadegrau 1"
+        return "com obesidadegrau 1."
     elif imc >= 35 and imc <= 40:
-        return "com obesidad grau 2"
+        return "com obesidad grau 2."
     elif imc < 40:
-        return "com obesidade grau 3"
+        return "com obesidade grau 3."
